cd19edd508a3db14859dd2bca8ca5521581e3652
rharhuandrew <rharhuky@gmail.com>
2023-03-05 16:47:57 -0300

    commit atualizacao

--- consultorio-dentista/src/classes_auxiliares/Constantes.java
+++ consultorio-dentista/src/classes_auxiliares/Constantes.java
@@ -13,4 +13,16 @@ public class Constantes {
 
 	public static Font tipoFonteTextoEsqueciMinhaSenha = new Font("Nimbus Sans", Font.PLAIN, 12);
 
+	public static int medidaXTelaApresentacao = 100;
+
+	public static int medidaYTelaApresentacao = 100;
+
+	public static int alturaTelaApresentacao = 499;
+	
+	public static int larguraTelaApresentacao = 561;
+
+	//////oi
+	
+	}
+	
 }
